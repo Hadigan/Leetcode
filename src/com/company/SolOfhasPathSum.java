4683219723f34e80585dbee955f48c84540f6c0e
Hadigan <894723062@qq.com>
2017-08-16 20:37:49 +0800

    解决上个问题的递归解法

--- src/com/company/SolOfhasPathSum.java
+++ src/com/company/SolOfhasPathSum.java
@@ -34,6 +34,12 @@ public class SolOfhasPathSum {
         return false;
     }
     public boolean hasPathSum(TreeNode root, int sum) {
-
+        if (root == null) return false;
+        if (root.left == null && root.right == null) {
+            return root.val == sum;
+        }
+        return hasPathSum(root.left, sum - root.val) ||
+                hasPathSum(root.right, sum - root.val);
     }
+
 }
