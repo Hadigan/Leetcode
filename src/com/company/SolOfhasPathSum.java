50f2e22ad56e062e295020d9ab3dabedd9dee5b6
Hadigan <894723062@qq.com>
2017-08-16 20:26:43 +0800

    解决判断是否有一条root->leaf的路径，使得路径上的所有值得和等于给定值。非递归方法

---
+++ src/com/company/SolOfhasPathSum.java
@@ -0,0 +1,39 @@
+package com.company;
+
+import java.util.Stack;
+
+public class SolOfhasPathSum {
+    public boolean hasPathSum2(TreeNode root, int sum) {
+
+        if (root == null) return false;
+        Stack<TreeNode> stack = new Stack<>();
+        Stack<Integer> stackVal = new Stack<>();
+        int fatherSum = 0;
+        TreeNode tmpTreeNode;
+        stack.push(root);
+        stackVal.push(new Integer(0));
+        while (!stack.isEmpty()) {
+            tmpTreeNode = stack.pop();
+            fatherSum = stackVal.pop();
+            if (tmpTreeNode.left == null && tmpTreeNode.right == null) {
+                if (fatherSum + tmpTreeNode.val == sum) {
+                    return true;
+                }
+            }
+            else {
+                if (tmpTreeNode.right != null) {
+                    stack.push(tmpTreeNode.right);
+                    stackVal.push(new Integer(fatherSum + tmpTreeNode.val));
+                }
+                if (tmpTreeNode.left != null) {
+                    stack.push(tmpTreeNode.left);
+                    stackVal.push(new Integer(fatherSum + tmpTreeNode.val));
+                }
+            }
+        }
+        return false;
+    }
+    public boolean hasPathSum(TreeNode root, int sum) {
+
+    }
+}
